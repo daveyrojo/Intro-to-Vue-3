4a46781b1344c74a9ffbe5d3b328dc7dc80a6d87
david eldridge <d.vincenteldridge@gmail.com>
2021-05-30 16:18:10 -0400

    finished L8

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const app = Vue.createApp({
             selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 80 },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
@@ -29,6 +29,11 @@ const app = Vue.createApp({
         },
         inStock() {
              return this.variants[this.selectedVariant].quantity;
+        },
+        onSale() {
+            if (this.variants[this.selectedVariant].quantity > 75) {
+                return `${this.title} are on Sale!`;
+            }
         }
     }
 })
