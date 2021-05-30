b9021d2d8ed70bf84f760cc4ceaa6c27e40a8458
david eldridge <d.vincenteldridge@gmail.com>
2021-05-29 22:23:41 -0400

    added sockItToMe url

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
-        }
+          product: "Socks",
+          image: "./assets/images/socks_green.jpg",
+          socks_url: "https://www.sockittome.com/",
+        };
     }
 })
